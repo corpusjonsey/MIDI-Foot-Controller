c37f10d65f52acbc566261cf1d3d9af599218ff5
corpusjonsey <corpusjonsey@gmail.com>
2016-06-29 23:46:18 -0600

    Update to v1.1.

--- DMC_6_Octatrack_1/DMC_6_Octatrack_1.1.ino
+++ DMC_6_Octatrack_1/DMC_6_Octatrack_1.1.ino
@@ -1,4 +1,4 @@
-/* DMC-6 Octatrack Firmware v1.0
+/* DMC-6 Octatrack Firmware v1.1
 
   1:  INAB rec #61
   2:  Pickup Play/Stop #64
@@ -7,10 +7,12 @@
   5:  Toggle Seq sync+start #71
   6:  Active track up #68
 
+  v1.1 - Updated to Bounce2 library, TODO: Expression control
+
 */
 
 #include <MIDI.h>
-#include <Bounce.h>
+#include <Bounce2.h>
 
 #define BTN1 2
 #define BTN2 3
@@ -26,19 +28,21 @@
 #define LED5 17
 #define LED6 13
 
+//#define EXP 19
+
 #define MIDI_CHANNEL 9 // MIDI channel = Octatrack autochannel 9
 
 int ledArray[] = {9, 14, 15, 16, 17, 13};
 int ledVal[] = {0, 0, 0, 0, 0, 0};
 int state = -1;
 
-// Instantiate six debounce objects, 25 millisecond bounce time
-Bounce bounce1 = Bounce(BTN1, 25);
-Bounce bounce2 = Bounce(BTN2, 25);
-Bounce bounce3 = Bounce(BTN3, 25);
-Bounce bounce4 = Bounce(BTN4, 25);
-Bounce bounce5 = Bounce(BTN5, 25);
-Bounce bounce6 = Bounce(BTN6, 25);
+// Instantiate six debounce objects
+Bounce bounce1 = Bounce();
+Bounce bounce2 = Bounce();
+Bounce bounce3 = Bounce();
+Bounce bounce4 = Bounce();
+Bounce bounce5 = Bounce();
+Bounce bounce6 = Bounce();
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
@@ -59,37 +63,44 @@ void setup() {
   pinMode(LED5, OUTPUT);
   pinMode(LED6, OUTPUT);
 
-  digitalWrite(BTN1, HIGH); //button initial state is "HIGH"
+  digitalWrite(BTN1, HIGH); //button initial state is "HIGH" (Internal Pullup)
   digitalWrite(BTN2, HIGH);
   digitalWrite(BTN3, HIGH);
   digitalWrite(BTN4, HIGH);
   digitalWrite(BTN5, HIGH);
   digitalWrite(BTN6, HIGH);
 
+  bounce1.attach(BTN1); //assign pins to bounce objects
+  bounce1.interval(10); //10ms stable interval
+  bounce2.attach(BTN2);
+  bounce2.interval(10);
+  bounce3.attach(BTN3);
+  bounce3.interval(10);
+  bounce4.attach(BTN4);
+  bounce4.interval(10);
+  bounce5.attach(BTN5);
+  bounce5.interval(10);
+  bounce6.attach(BTN6);
+  bounce6.interval(10);
+
   //LED light show at startup.
   for (int i = 0; i < 3; i++) {
     digitalWrite(LED1, HIGH);
-    delay(10);
-    digitalWrite(LED4, LOW);
-    delay(100);
-    digitalWrite(LED2, HIGH);
-    delay(10);
-    digitalWrite(LED1, LOW);
-    delay(100);
-    digitalWrite(LED3, HIGH);
-    delay(10);
-    digitalWrite(LED2, LOW);
-    delay(100);
     digitalWrite(LED6, HIGH);
     delay(10);
     digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
     delay(100);
+    digitalWrite(LED2, HIGH);
     digitalWrite(LED5, HIGH);
     delay(10);
+    digitalWrite(LED1, LOW);
     digitalWrite(LED6, LOW);
     delay(100);
+    digitalWrite(LED3, HIGH);
     digitalWrite(LED4, HIGH);
     delay(10);
+    digitalWrite(LED2, LOW);
     digitalWrite(LED5, LOW);
     delay(100);
   }
@@ -101,7 +112,7 @@ void loop() {
     digitalWrite(ledArray[i], ledVal[i]); //update LEDs
     ledVal[i] = 0;
   }
-  delay(100);
+  delay(50);
   for (int i = 0; i < 6; i++) {
     digitalWrite(ledArray[i], ledVal[i]); //turn off LEDs
   }
@@ -157,6 +168,6 @@ void loop() {
 
 void midiNote(int note) {
   MIDI.sendNoteOn(note, 127, MIDI_CHANNEL);
-  delay(5);
+  delay(10);
   MIDI.sendNoteOff(note, 0, MIDI_CHANNEL);
 }
